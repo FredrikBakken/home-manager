72d05649c607e8681046024a98169c85b47aec26
Fredrik Bakken <Fredrik.Bakken@norges-bank.no>
2026-03-17 09:57:24 +0100

    chore(work): remove unused work tools
    Signed-off-by: Fredrik Bakken <Fredrik.Bakken@norges-bank.no>

--- flake.nix
+++ flake.nix
@@ -4,13 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/master";
+    home-manager = {
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
